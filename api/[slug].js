02252e53e53861aace557a789680b7d49cf655d0
omniharmonic <155489756+omniharmonic@users.noreply.github.com>
2025-06-26 03:12:22 -0600

    Add one-level dynamic API route for Vercel dynamic routing test

---
+++ api/[slug].js
@@ -0,0 +1,11 @@
+module.exports = async function handler(req, res) {
+  const { slug } = req.query;
+  res.status(200).json({
+    message: "[slug] route hit!",
+    slug,
+    method: req.method,
+    query: req.query,
+    url: req.url,
+    headers: req.headers
+  });
+}; 
